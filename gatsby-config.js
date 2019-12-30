82017627cc13acd74d2a4aa589faa8f95b531b9b
yceffort <yceffort@gmail.com>
2019-12-30 17:49:36 +0900

    [#007] 💄change font

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,10 +81,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Raleway`,
-          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-        ],
+        fonts: [`Raleway`],
       },
     },
     {
