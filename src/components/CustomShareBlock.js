3d9973e9470d4e2f4703621bcf114813a480f684
yceffort <yceffort@gmail.com>
2020-01-02 17:47:09 +0900

    [#37] ☕️ add buy me a coffee

--- src/components/CustomShareBlock.js
+++ src/components/CustomShareBlock.js
@@ -1,43 +1,56 @@
-import PropTypes from "prop-types";
-import React from "react";
+import PropTypes from "prop-types"
+import React from "react"
 
-import { FaTwitter, FaLinkedin, FaFacebook, FaEnvelope } from "react-icons/fa";
-import { ShareBlockStandard, ShareButtonIconOnly } from "react-custom-share";
+import { FaTwitter, FaLinkedin, FaFacebook, FaEnvelope } from "react-icons/fa"
+import { ShareBlockStandard, ShareButtonIconOnly } from "react-custom-share"
+
+import "./CustomShareBlock.css"
 
 const CustomShareBlock = props => {
-    const { url, title, siteName } = props;
+  const { url, title, siteName } = props
 
-    const shareBlockProps = {
-        url: url,
-        button: ShareButtonIconOnly,
-        buttons: [
-            { network: "Twitter", icon: FaTwitter },
-            { network: "Facebook", icon: FaFacebook },
-            { network: "Linkedin", icon: FaLinkedin },
-            { network: "Email", icon: FaEnvelope }
-        ],
-        text: title,
-        longtext: siteName
-    };
-    return (
-        <div className="mt-4">
-            <ShareBlockStandard {...shareBlockProps} />
-            <p className="text-center"><i>If you like it, share it!</i></p>
-        </div>
-    )
-};
+  const shareBlockProps = {
+    url: url,
+    button: ShareButtonIconOnly,
+    buttons: [
+      { network: "Twitter", icon: FaTwitter },
+      { network: "Facebook", icon: FaFacebook },
+      { network: "Linkedin", icon: FaLinkedin },
+      { network: "Email", icon: FaEnvelope },
+    ],
+    text: title,
+    longtext: siteName,
+  }
+  return (
+    <div className="mt-4">
+      <ShareBlockStandard {...shareBlockProps} />
+      <p className="text-center">
+        <a
+          class="bmc-button"
+          target="_blank"
+          href="https://www.buymeacoffee.com/foryeffort"
+        >
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+            alt="Buy me a coffee"
+          />
+          <span>Buy me a coffee</span>
+        </a>
+      </p>
+    </div>
+  )
+}
 
 CustomShareBlock.propTypes = {
-    url: PropTypes.string,
-    title: PropTypes.string,
-    siteName: PropTypes.string
-};
+  url: PropTypes.string,
+  title: PropTypes.string,
+  siteName: PropTypes.string,
+}
 
 CustomShareBlock.defaultProps = {
-    url: "https://mywebsite.com/page-to-share/",
-    title: "Default value of title",
-    siteName: "Default value of excerpt"
-};
+  url: "https://mywebsite.com/page-to-share/",
+  title: "Default value of title",
+  siteName: "Default value of excerpt",
+}
 
-export default CustomShareBlock;
-
+export default CustomShareBlock
