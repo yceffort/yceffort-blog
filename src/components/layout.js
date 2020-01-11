4eb12b348c7db0a08c4af36e5863d4ddc8361104
yceffort <yceffort@gmail.com>
2020-01-11 20:00:38 +0900

    [#8] 👕 add styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,9 +41,13 @@ const Layout = ({ children }) => {
           <div
             style={{
               margin: `0 auto`,
-              padding: `0px 1.0875rem 1.45rem`,
               paddingTop: 0,
               wordBreak: "break-all",
+            }}
+            ref={node => {
+              if (node) {
+                node.style.setProperty("padding", "1rem", "important")
+              }
             }}
           >
             <main
