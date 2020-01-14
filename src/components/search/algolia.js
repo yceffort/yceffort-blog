3732d49e453edd1d6a1cc7dd4168fd6664d1d988
yceffort <yceffort@gmail.com>
2020-01-14 10:04:17 +0900

    [#67] 🐛 add slug in aloglia

--- src/components/search/algolia.js
+++ src/components/search/algolia.js
@@ -1,24 +1,28 @@
 const postQuery = `{
-  posts: allMarkdownRemark(
-    filter: { fileAbsolutePath: { regex: "/content/" } }
-  ) {
+  posts: allMarkdownRemark(filter: {}) {
     edges {
       node {
         objectID: id
+        timeToRead
         frontmatter {
           title
           date(formatString: "YYYY-MM-DD")
           tags
         }
-        excerpt(pruneLength: 5000)
+        excerpt(pruneLength: 3000)
+        fields {
+          slug
+        }
       }
     }
   }
-}`
+}
+`
 
 const flatten = arr =>
-  arr.map(({ node: { frontmatter, ...rest } }) => ({
+  arr.map(({ node: { frontmatter, fields: { slug }, ...rest } }) => ({
     ...frontmatter,
+    slug,
     ...rest,
   }))
 const settings = { attributesToSnippet: [`excerpt:100`] }
