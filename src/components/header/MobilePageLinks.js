6f8ffbff5dd81934df31483af0b6da6189b63783
jayg <jayg@triple-corp.com>
2020-06-02 19:59:05 +0900

    [#120] javascript 10 question

--- src/components/header/MobilePageLinks.js
+++ src/components/header/MobilePageLinks.js
@@ -5,17 +5,17 @@ const MobilePages = () => {
   return (
     <div className="mobile-pages-main">
       <div className="text-center">
-        <p className="d-inline p-4">
+        <p className="d-inline p-44">
           <Link to="/">
             <span className="text-dark">Home</span>
           </Link>
         </p>
-        <p className="d-inline p-4">
+        <p className="d-inline p-44">
           <Link to="/about">
             <span className="text-dark">About</span>
           </Link>
         </p>
-        <p className="d-inline p-4">
+        <p className="d-inline p-44">
           <Link to="/archive">
             <span className="text-dark">Archive</span>
           </Link>
